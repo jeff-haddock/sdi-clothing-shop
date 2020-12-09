7259829e395a90b8380e4cf56e7bed10e72b41cf
Jeff Haddock <jeff.haddock.developer@gmail.com>
2020-12-09 12:11:40 -0500

    added header and converted App to class

--- sdi-clothing-shop/src/App.js
+++ sdi-clothing-shop/src/App.js
@@ -1,12 +1,28 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>SDI Clothing Shop</h1>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state ={
+      
+    }
+
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <div className="nav-header">I'm the header</div>
+        <div className="main-area">
+          <h1>SDI Clothing Shop</h1>
+        </div>
+      
+      </div>
+    )
+  ;}
 }
 
 export default App;
