aab4b5c27d99b71e5da7f08778883ca8e18913ec
Jeff Haddock <jeff.haddock.developer@gmail.com>
2020-12-09 11:09:41 -0500

    initial application commit with create-react-app

---
+++ sdi-clothing-shop/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>SDI Clothing Shop</h1>
+    </div>
+  );
+}
+
+export default App;
